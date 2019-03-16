9be73e344a30f1e6531be3a68a4148ec9ab3ee3a
rhoang57 <rhoang57@gmail.com>
2019-03-15 22:35:40 -0700

    adding methods to NewTextEditor component

--- client/src/pages/BrowseWrite.js
+++ client/src/pages/BrowseWrite.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewBlockPreview from "../components/NewBlockPreview";
 import WriteCard from "../components/WriteCard";
 import { Container, Row, Col } from "../components/Grid";
+import API from "../utils/API";
 
 class BrowseWrite extends Component {
     
@@ -9,14 +10,14 @@ class BrowseWrite extends Component {
         pieces: []
       }
     
-      // componentDidMount() {
-      //   this.loadiUnfnishedPieces();
-      // }
+      componentDidMount() {
+        this.loadUnfinishedPieces();
+      }
     
-      // loadUnfinishedPieces() {
-      //   API.getUnfinishedPieces()
-      //     .then(res => this.setState({ pieces: res.data }));
-      // }
+      loadUnfinishedPieces() {
+        API.getUnfinishedPieces()
+          .then(res => this.setState({ pieces: res.data }));
+      }
 
 render() {
     return (
@@ -26,18 +27,17 @@ render() {
             <Col size="md-4">
               <NewBlockPreview newblocklink={"/write/new"} />
             </Col>
-            <Col size="md-4">
             {this.state.pieces.map(piece => (
-          <WriteCard
+            <Col size="md-4">
+            <WriteCard
             id={piece._id}
             key={piece._id}
             title={piece.title}
             excerpt={piece.blocks[piece.blocks.length-1].text}
             authorCount={piece.authorCount}
           />
-        ))}
             </Col>
-            
+            ))}
           </Row>
         </Container>
       </>
--- client/src/pages/NewTextEditor.js
+++ client/src/pages/NewTextEditor.js
@@ -3,10 +3,7 @@ import { Container, Row, Col } from "../components/Grid";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import API from "../utils/API";
 
-
-
 class NewTextEditor extends Component {
-  
   state = {
     title: "",
     text: ""
@@ -21,7 +18,24 @@ class NewTextEditor extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-  }
+    const pieceData = {
+      title: this.state.title,
+      blocks: [],
+      authorCount: 0
+    };
+    console.log("Alright");
+    API.createPiece(pieceData)
+      .then(res => {
+        const blockData = {
+          text: this.state.text,
+          pieceId: res.data._id
+        };
+        API.createBlock(blockData)
+          .then(res => console.log(res))
+          .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
+  };
 
   render() {
     return (
@@ -29,26 +43,26 @@ class NewTextEditor extends Component {
         <Container fluid>
           <Row>
             <Col size="md-6">
-            <form>
-            <Input
-                value={this.state.title}
-                onChange={this.handleInputChange}
-                name="title"
-                placeholder="Title (required)"
-              />
-              <TextArea
-                value={this.state.text}
-                onChange={this.handleInputChange}
-                name="text"
-                placeholder="Add your portion of the story here (between 10 and 150 words)"
-              />
-              <FormBtn
-                disabled={!(this.state.title && this.state.text)}
-                onClick={this.handleFormSubmit}
-              >
-                Submit Starting Block
-              </FormBtn>
-            </form>
+              <form>
+                <Input
+                  value={this.state.title}
+                  onChange={this.handleInputChange}
+                  name="title"
+                  placeholder="Title (required)"
+                />
+                <TextArea
+                  value={this.state.text}
+                  onChange={this.handleInputChange}
+                  name="text"
+                  placeholder="Add your portion of the story here (between 10 and 150 words)"
+                />
+                <FormBtn
+                  disabled={!(this.state.title && this.state.text)}
+                  onClick={this.handleFormSubmit}
+                >
+                  Submit Starting Block
+                </FormBtn>
+              </form>
             </Col>
           </Row>
         </Container>
